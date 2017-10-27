fed246653113927668648ff7fa8e445af474f27f
Jawaad Mahmood <jmahmood@salesforce.com>
2017-10-27 13:12:20 +0900

    Remote serving / no easy killing of sleeping processes

--- app/server.py
+++ app/server.py
@@ -2,11 +2,16 @@
 import logging
 import time
 
+from tornado import ioloop
 from tornado.web import RequestHandler, Application
 import tornado.ioloop
 import tornado.options
 import tornado.wsgi
 import datetime
+import requests
+import os
+
+REMOTESERVER = '127.0.0.1:9999'
 
 class BaseRequest(RequestHandler):
     def data_received(self, chunk):
@@ -16,10 +21,10 @@ class BaseRequest(RequestHandler):
         pass
 
     def get(self, *args, **kwargs):
-        print('[{0}] Receive Request ({1})'.format(datetime.datetime.now(), self.__class__.__name__))
+        # print('[{0}] Receive Request ({1})'.format(datetime.datetime.now(), self.__class__.__name__))
         self.wait_period()
         self.write('Request Complete')
-        print('[{0}] Serve Request ({1})'.format(datetime.datetime.now(), self.__class__.__name__))
+        # cprint('[{0}] Serve Request ({1})'.format(datetime.datetime.now(), self.__class__.__name__))
 
 
 class ThirtySecondSecondRequest(BaseRequest):
@@ -42,12 +47,38 @@ class OneSecondSecondRequest(BaseRequest):
         time.sleep(1)
 
 
+class BaseRemoteServer(RequestHandler):
+    url=''
+
+    def get(self, *args, **kwargs):
+        print('[{0}] Receive Remote Request ({1})'.format(datetime.datetime.now(), self.__class__.__name__))
+        self.write(requests.get(self.url.format(REMOTESERVER)).content)
+        print('[{0}] Serve Remote Request ({1})'.format(datetime.datetime.now(), self.__class__.__name__))
+
+
+class RemoteTwentySecondSecondRequest(BaseRemoteServer):
+    url = 'http://{0}/20'.format(REMOTESERVER)
+
+
+class RemoteTenSecondSecondRequest(BaseRemoteServer):
+    url = 'http://{0}/10'.format(REMOTESERVER)
+
+
+class RemoteOneSecondSecondRequest(BaseRemoteServer):
+    url = 'http://{0}/1'.format(REMOTESERVER)
+
+
 def generate_application():
     return Application([
-        (r"/1", OneSecondSecondRequest),  # Waits 1 second and then returns some text.
-        (r"/10", TenSecondSecondRequest),  # Waits 10 seconds and then returns some text.
-        (r"/20", TwentySecondSecondRequest),  # Waits 20 seconds and then returns some text.
-        (r"/30", ThirtySecondSecondRequest),  # Waits 30 seconds and then returns some text.
+        (r"/remote/1$", RemoteOneSecondSecondRequest),  # Contacts a remote server and returns about 1 second later.
+        (r"/remote/10$", RemoteTenSecondSecondRequest),  # Contacts a remote server and returns about 10 seconds later.
+        (r"/remote/20$", RemoteTwentySecondSecondRequest),  # Contacts a remote server and returns about 20 seconds later.
+        (r"/1$", OneSecondSecondRequest),  # Waits 1 second and then returns some text.
+        (r"/10$", TenSecondSecondRequest),  # Waits 10 seconds and then returns some text.
+        (r"/20$", TwentySecondSecondRequest),  # Waits 20 seconds and then returns some text.
+        (r"/30$", ThirtySecondSecondRequest),  # Waits 30 seconds and then returns some text.
+
+
     ], DEBUG=True)
 
 
--- app/start.sh
+++ app/start.sh
@@ -1,1 +1,30 @@
-GUNICORN_CMD_ARGS="--bind=127.0.0.1 --workers=3 --log-level=debug" gunicorn -k tornado server:app
+#!/bin/bash
+
+GUNICORN_CMD_ARGS="--bind=127.0.0.1:9999 --workers=3 --log-level=warning" gunicorn -k tornado server:app &
+BACKGROUND_SERVER_PID=$!
+
+GUNICORN_CMD_ARGS="--bind=127.0.0.1:8000 --workers=3 --log-level=debug" gunicorn -k tornado server:app &
+FOREGROUND_SERVER_PID=$!
+
+sleep 5s
+
+# This should serve normally.
+/usr/bin/curl -s http://127.0.0.1:8000/remote/1 > /dev/null &
+
+# This should return normally after it is killed.
+/usr/bin/curl -s http://127.0.0.1:8000/remote/10 > /dev/null &
+
+sleep 5s
+
+# The foreground parent process should be killed; any request to this url should be ignored and not logged.
+kill $FOREGROUND_SERVER_PID
+sleep 2s
+
+# This should not be received!  It should not show up in the log as it happened after the sigterm.
+/usr/bin/curl -s http://127.0.0.1:8000/remote/20 > /dev/null &
+
+# Wait to see if anything happens.
+sleep 30s
+
+# Once all the excitement is done, kill the silent background server.
+kill $BACKGROUND_SERVER_PID
