ef2c6ce05f7e1f13d64a81b57d8ef3ef83cc332e
Jawaad Mahmood <jmahmood@salesforce.com>
2017-10-30 11:59:19 +0900

    Simulation added to start.sh file.

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 A way to test GUnicorn timeout behavior.
 
-To run, type in `bash start.sh`
+To run, type in `bash start.sh`.  It is assumed that the version of python you are running has the appropriate libraries already installed. (`pip install -r requirements.txt`)
+
+This starts a foreground and background version of the server.
+
+The servers themselves have commands that return 1, 10, 20 and 30 second requests.  
+
+The experiment starts a gunicorn server that is attached to a basic tornado application.  
+
 
 The urls you are interested are all GET urls.
 
--- app/start.sh
+++ app/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-GUNICORN_CMD_ARGS="--bind=127.0.0.1:9999 --workers=3 --log-level=warning graceful_timeout=30" gunicorn -k tornado server:app &
+GUNICORN_CMD_ARGS="--bind=127.0.0.1:9999 --workers=3 --log-level=warning --graceful-timeout 30" gunicorn -k tornado server:app &
 BACKGROUND_SERVER_PID=$!
 
-GUNICORN_CMD_ARGS="--bind=127.0.0.1:8000 --workers=3 --log-level=debug graceful_timeout=30" gunicorn -k tornado server:app &
+GUNICORN_CMD_ARGS="--bind=127.0.0.1:8000 --workers=3 --log-level=debug --graceful-timeout 20" gunicorn -k tornado server:app &
 FOREGROUND_SERVER_PID=$!
 
 sleep 5s
@@ -12,19 +12,16 @@ sleep 5s
 /usr/bin/curl -s http://127.0.0.1:8000/remote/1 > /dev/null &
 
 # This should return normally after it is killed.
-/usr/bin/curl -s http://127.0.0.1:8000/remote/10 > /dev/null &
+/usr/bin/curl  -s -D - http://127.0.0.1:8000/remote/10 -o /dev/null &
 
-sleep 3s
+sleep 2s
 
 # The foreground parent process should be killed; any request to this url should be ignored and not logged.
 kill $FOREGROUND_SERVER_PID
 sleep 2s
 
 # This should not be received!  It should not show up in the log as it happened after the sigterm.
-/usr/bin/curl -s http://127.0.0.1:8000/remote/20 > /dev/null &
-
-# Wait to see if anything happens.
-sleep 30s
+/usr/bin/curl  -s -D - http://127.0.0.1:8000/remote/20  -o /dev/null
 
 # Once all the excitement is done, kill the silent background server.
 kill $BACKGROUND_SERVER_PID
